4bcc6c1d238ae308e1b26dfb0aab07438d546978
Soumen Samanta <83171626+novamaster-git@users.noreply.github.com>
2022-01-11 14:59:18 +0530

    Update Promise-handle-using-async-await.js

--- Promise/Promise-handle-using-async-await.js
+++ Promise/Promise-handle-using-async-await.js
@@ -1,40 +1,38 @@
 // The purpose of this code is to demonstrate the promise handling using async await 
-function promiseRunner(data){
-  return new Promise((resolve, reject) => {
-    if(data){
-    console.log("Promise Solved");
-    resolve("solved");
-    }else{
-    console.log("Promise reject");
-    reject("rejected");
-    }
-  })
+function promiseRunner(data) {
+	return new Promise((resolve, reject) => {
+		if (data) {
+			console.log("Promise Solved");
+			resolve("solved");
+		} else {
+			console.log("Promise reject");
+			reject("rejected");
+		}
+	})
 }
 
-async function startResolve(){
-  try{
-  console.log("Starting....");
-  const data = await promiseRunner(true);
-  console.log(data);
-  }
-  catch(e){
-    console.log(e);
-  }
+async function startResolve() {
+	try {
+		console.log("Starting....");
+		const data = await promiseRunner(true);
+		console.log(data);
+	} catch (e) {
+		console.log(e);
+	}
 }
-// startResolve();
+startResolve();
 // Starting....
 // Promise Solved
 // solved
 
-async function startReject(){
-  try{
-  console.log("Starting....");  
-  const data = await promiseRunner(false);
-  console.log("fd" + data);
-  }
-  catch(e){
-    console.log("Error : " + e);
-  }
+async function startReject() {
+	try {
+		console.log("Starting....");
+		const data = await promiseRunner(false);
+		console.log("fd" + data);
+	} catch (e) {
+		console.log("Error : " + e);
+	}
 }
 
 startReject();
